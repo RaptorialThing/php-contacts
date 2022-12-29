6cf96015c0e81ca739590f757f6a028c3c7f729d
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2022-12-29 22:20:43 +0300

    added new version

--- app/Exports/ContactsExport.php
+++ app/Exports/ContactsExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Contact;
+use App\Models\Contact;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Contracts\Support\Responsable;
 use Maatwebsite\Excel\Concerns\Exportable;
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Contact;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use App\Exports\ContactsExport;
 use Maatwebsite\Excel\Facades\Excel;
